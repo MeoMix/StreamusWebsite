90892203badeb7f3e13bba1fae1fc879c7aa1ac5
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2013-11-29 08:38:44 -0800

    Finally got node_modules added

--- Gruntfile.js
+++ Gruntfile.js
@@ -80,7 +80,7 @@ module.exports = function (grunt) {
 	
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	//grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
