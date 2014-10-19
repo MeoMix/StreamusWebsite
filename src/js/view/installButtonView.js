3d05042f6de4a2e9550fe5af6058299d4d00cc93
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2014-10-19 13:42:00 -0700

    Updated install button for opera

--- src/js/view/installButtonView.js
+++ src/js/view/installButtonView.js
@@ -9,6 +9,7 @@
         },
         
         webstoreUrl: 'https://chrome.google.com/webstore/detail/jbnkffmindojffecdhbbmekbmkkfpmjd',
+        operaExtensionId: 'nnmcpagedcgekmljdamaeahfbmmjloho',
 
         initialize: function () {
 
@@ -20,40 +21,47 @@
 
             var browserIsNotChrome = navigator.userAgent.toLowerCase().indexOf('chrome') === -1;
             if (browserIsNotChrome) {
-                this.$el.attr('disabled', true).text('Google Chrome required');
+                this.$el.attr('disabled', true).text('Google Chrome or Opera required');
                 return;
             }
 
-            //  Check version of Chrome. Streamus requires v29+ currently:
+            //  Check version of Chrome. Streamus requires v32+ currently:
             var majorVersion = parseInt(window.navigator.appVersion.match(/Chrome\/(\d+)\./)[1], 10);
 
             if (majorVersion < 29) {
-                this.$el.attr('disabled', true).text('Google Chrome v29+ required. Please update.');
+                this.$el.attr('disabled', true).text('Google Chrome v32+ required. Please update.');
             }
 
         },
 
         install: function () {
-
             if (!this.$el.attr('disabled')) {
-
                 this.$el.attr('disabled', true).text('Installing...');
 
-                var self = this;
-                chrome.webstore.install(this.webstoreUrl, function () {
-                    self.$el.text('Installed!');
-                    self.$el.trigger('installed');
-                }, function (error) {
-                    //  TODO: It would be nice to know more about where this error message came from / what others could occur.
-                    if (error == 'User cancelled install') {
-                        self.$el.attr('disabled', false).text('Install extension now');
-                    } else {
-                        self.$el.text('Error: ' + error);
-                    }
-
-                });
+                if (this._isBrowserOpera()) {
+                    opr.addons.installExtension(this.operaExtensionId, this._onInstallSuccess.bind(this), this._onInstallError.bind(this));
+                } else {
+                    chrome.webstore.install(this.webstoreUrl, this._onInstallSuccess.bind(this), this._onInstallError.bind(this));
+                }
             }
-
+        },
+        
+        _onInstallSuccess: function() {
+            this.$el.text('Installed!');
+            this.$el.trigger('installed');
+        },
+        
+        _onInstallError: function(error) {
+            //  TODO: It would be nice to know more about where this error message came from / what others could occur.
+            if (error == 'User cancelled install') {
+                this.$el.attr('disabled', false).text('Install extension now');
+            } else {
+                this.$el.text('Error: ' + error);
+            }
+        },
+        
+        _isBrowserOpera: function() {
+            return navigator.userAgent.indexOf(' OPR/') >= 0;  
         },
         
         onInstalled: function(event) {
