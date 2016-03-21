8ba6103e4150ee1842216cd27b6e9f7a38cee33d
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2016-03-21 11:40:03 -0700

    Simplify Karma configuration
    This is now possible with JSPM v0.17. Hooray.

--- jspm/jspm.browser.js
+++ jspm/jspm.browser.js
@@ -1,5 +1,6 @@
 SystemJS.config({
-  baseURL: '/',
+  // KarmaJS runs everything from a sub-directory.
+  baseURL: window.__karma__ ? '/base/' : '/',
   paths: {
     'github:*': 'jspm/jspm_packages/github/*',
     'npm:*': 'jspm/jspm_packages/npm/*'
--- karma.config.js
+++ karma.config.js
@@ -1,22 +1,4 @@
 /* global process */
-var fs = require('fs');
-var path = require('path');
-var _ = require('lodash');
-
-// Determine all potential paths which need to be proxied by KarmaJS
-// Replace requests to '/proxyTarget' with '/base/proxyTarget'
-var proxyTargets = fs.readdirSync('compiled/').filter(function(file) {
-  return fs.statSync(path.join('compiled/', file)).isDirectory();
-});
-
-var base = '/base/compiled';
-var proxyValues = _.map(proxyTargets, function(proxyTarget) {
-  return base + '/' + proxyTarget + '/';
-});
-
-var proxies = _.keyBy(proxyValues, function(proxyValue) {
-  return proxyValue.replace(base, '');
-});
 
 module.exports = function(config) {
   var configuration = {
@@ -34,24 +16,14 @@ module.exports = function(config) {
       serveFiles: ['compiled/**/*'],
       paths: {
         // Need to redirect a path such as 'common/route' to 'base/compiled/common/route'
-        '*': 'base/compiled/*',
+        '*': 'compiled/*',
         // Tests aren't located under compiled directory. Undo the above path modification.
         'compiled/*': 'compiled/*',
         // TODO: .js file-ending necessary due to bug with karma-jspm: https://github.com/Workiva/karma-jspm/issues/141
         'test/*': 'test/*.js'
       }
     },
-    
-    // Karma serves all files under a /base/ directory.
-    // This conflicts with the baseURL param provided through jspm's configuration file.
-    // So, proxy requests to known paths through /base/ to keep everything working.
-    // Don't proxy '/' because an infinite loop will occur when looking up non-trivial paths.
-    proxies: _.defaults({
-      '/compiled/': '/base/compiled/',
-      '/test/': '/base/test/',
-      '/jspm_packages/': '/base/jspm_packages/'
-    }, proxies),
-    
+
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
