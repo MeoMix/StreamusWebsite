a54a95693696d0cbfd7fd31977360a77e9853e75
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2016-03-21 11:20:57 -0700

    Increase timeout

--- karma.config.js
+++ karma.config.js
@@ -77,6 +77,8 @@ module.exports = function(config) {
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: true,
+
+    browserNoActivityTimeout: 20000,
     
     customLaunchers: {
       Chrome_travis_ci: {
